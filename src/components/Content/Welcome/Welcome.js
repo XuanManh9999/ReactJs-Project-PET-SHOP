4937fed0a52a0f857ed573c24b876e1541f67a99
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2023-12-06 16:07:41 +0700

    Fix bug and add fontawsome

--- src/components/Content/Welcome/Welcome.js
+++ src/components/Content/Welcome/Welcome.js
@@ -1,13 +1,17 @@
 import clsx from "clsx";
 import styles from "./Welcome.module.scss";
-// import { FaDong  } from "react-icons/fa";
-
+import { FaDollarSign } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDongSign } from "@fortawesome/free-solid-svg-icons";
 function Welcome() {
     return (
         <>
             <div>
                 <h1>Welcome</h1>
-                <i>{/* <FaDong /> */}</i>
+                <i>
+                    <FaDollarSign />
+                </i>
+                <FontAwesomeIcon icon={faDongSign} />
             </div>
         </>
     );
