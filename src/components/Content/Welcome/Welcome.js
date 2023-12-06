4e15f753529f69bf4af720e6558d793397cb1f14
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2023-12-06 22:40:56 +0700

    Add Infor

--- src/components/Content/Welcome/Welcome.js
+++ src/components/Content/Welcome/Welcome.js
@@ -1,19 +1,85 @@
+import { Link } from "react-router-dom";
+
 import clsx from "clsx";
 import styles from "./Welcome.module.scss";
-import { FaDollarSign } from "react-icons/fa";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDongSign } from "@fortawesome/free-solid-svg-icons";
+
 function Welcome() {
     return (
-        <>
-            <div>
-                <h1>Welcome</h1>
-                <i>
-                    <FaDollarSign />
-                </i>
-                <FontAwesomeIcon icon={faDongSign} />
+        <div className={clsx(styles.container)}>
+            <div className={clsx(styles.infor)}>
+                <h1 className={clsx(styles.title)}>
+                    Chào mừng bạn đến với PET SHOP.
+                </h1>
+                <div className={clsx(styles.img_infor)}>
+                    <img
+                        src="images/hero_animal.png"
+                        alt=" Chào mừng bạn đến với PET SHOP."
+                    />
+                </div>
             </div>
-        </>
+            <div className={clsx(styles.list_content_hero)}>
+                {/* Item 1 */}
+                <div className={clsx(styles.item_hero)}>
+                    <Link to={""}>
+                        <picture className={clsx(styles.img_hero)}>
+                            <img src="images/img-hero-dog.webp" alt="" />
+                        </picture>
+                    </Link>
+                    <h2 className={clsx(styles.title_item_hero)}>
+                        <Link to={""}>Dành cho chó</Link>
+                    </h2>
+                    <p className={clsx(styles.desc_hero, "line-clamp-3")}>
+                        Chó cưng, đồ chơi cho chó và các sản phẩm chuyên dành
+                        cho chó cưng.
+                    </p>
+                </div>
+                {/* Item 2 */}
+                <div className={clsx(styles.item_hero)}>
+                    <Link to={""}>
+                        <picture className={clsx(styles.img_hero)}>
+                            <img src="images/img-hero-cat.webp" alt="" />
+                        </picture>
+                    </Link>
+                    <h2 className={clsx(styles.title_item_hero)}>
+                        <Link to={""}>Dành cho Mèo</Link>
+                    </h2>
+                    <p className={clsx(styles.desc_hero, "line-clamp-3")}>
+                        Mèo cưng, đồ chơi cho mèo và các sản phẩm chuyên dành
+                        cho mèo cưng.
+                    </p>
+                </div>
+                {/* Item 3 */}
+                <div className={clsx(styles.item_hero)}>
+                    <Link to={""}>
+                        <picture className={clsx(styles.img_hero)}>
+                            <img src="images/img-hero-hamter.webp" alt="" />
+                        </picture>
+                    </Link>
+                    <h2 className={clsx(styles.title_item_hero)}>
+                        <Link to={""}>Dành cho Hamster</Link>
+                    </h2>
+                    <p className={clsx(styles.desc_hero, "line-clamp-3")}>
+                        Hamster cưng, đồ chơi cho Hamster và các sản phẩm chuyên
+                        dành cho Hamster.
+                    </p>
+                </div>
+                {/* Item 4 */}
+                <div className={clsx(styles.item_hero)}>
+                    <Link to={""}>
+                        <picture className={clsx(styles.img_hero)}>
+                            <img src="images/img-hero-bird.webp" alt="" />
+                        </picture>
+                    </Link>
+                    <h2 className={clsx(styles.title_item_hero)}>
+                        <Link to={""}>Dành cho Chim</Link>
+                    </h2>
+                    <p className={clsx(styles.desc_hero, "line-clamp-3")}>
+                        Chim cưng, đồ chơi cho chim và các sản phẩm chuyên dành
+                        cho chim cưng.
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
