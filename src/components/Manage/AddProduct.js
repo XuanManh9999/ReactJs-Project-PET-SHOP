19dd62a683bd1cd7ea32f446d48cfd3000bcf572
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-01-12 12:44:36 +0700

    Add Hendle Admin

--- src/components/Manage/AddProduct.js
+++ src/components/Manage/AddProduct.js
@@ -1,10 +1,8 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "animate.css/animate.min.css";
-
 import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
 import "datatables.net-bs5/js/dataTables.bootstrap5.min.js";
-
 import "./assets/css/style.css";
 import "./assets/vendor/bootstrap/css/bootstrap.min.css";
 import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
@@ -13,12 +11,135 @@ import "./assets/vendor/quill/quill.snow.css";
 import "./assets/vendor/quill/quill.bubble.css";
 import "./assets/vendor/remixicon/remixicon.css";
 import "./assets/vendor/simple-datatables/style.css";
-
-import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./Header.module.scss";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// markdown
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
 
 function AddProduct() {
+  const [productData, setProductData] = useState({
+    name: "",
+    desc: "",
+    descHTML: "",
+    avatar: "",
+    price: 0,
+    salePrice: 0,
+    manyProducts: 0,
+    comment: "",
+    trademark: "",
+    detailImages: [
+      // { hrefImage: "link_to_detail_image_2.jpg" },
+    ],
+    sizes: [],
+    colors: [],
+  });
+
+  const [detailImages, setDetailImage] = useState("");
+  const [saveImages, setSaveImages] = useState([]);
+  const [detailColor, setDetailColor] = useState("");
+  const [saveColor, setSaveColor] = useState([]);
+  const [detailSize, setDetailSize] = useState("xs");
+  const [saveSize, setSaveSize] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProductData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  function handleEditorChange({ html, text }) {
+    setProductData((prevData) => ({
+      ...prevData,
+      descHTML: html,
+      desc: text,
+    }));
+  }
+
+  let hendleImageDetail = () => {
+    if (detailImages !== "") {
+      setSaveImages((prev) => [...prev, { hrefImage: detailImages }]);
+      setDetailImage("");
+      toast.success(
+        "Thêm dữ liệu thành công. Sau khi thêm đủ đừng quên lưu nhé"
+      );
+    } else {
+      toast.warn("Không thể thêm urlImage rỗng. Vui lòng thao tác lại");
+    }
+  };
+
+  let hendleSaveDetailImage = () => {
+    if (saveImages && saveImages.length > 0) {
+      setProductData((prev) => ({
+        ...prev,
+        detailImages: saveImages,
+      }));
+      toast.success("Lưu thành công");
+    } else {
+      toast.warn("Không thể lưu dữ liệu rỗng");
+    }
+  };
+
+  let hendleColorDetail = () => {
+    if (detailColor !== "") {
+      setSaveColor((prev) => [...prev, { color: detailColor }]);
+      setDetailColor("");
+      toast.success(
+        "Thêm dữ liệu thành công. Sau khi thêm đủ đừng quên lưu nhé"
+      );
+    } else {
+      toast.warn("Không thể thêm trường rỗng. Vui lòng thao tác lại");
+    }
+  };
+
+  let hendleSaveColor = () => {
+    if (saveColor && saveColor.length > 0) {
+      setProductData((prev) => ({
+        ...prev,
+        colors: saveColor,
+      }));
+      toast.success("Lưu thành công");
+    } else {
+      toast.warn("Không thể lưu dữ liệu rỗng");
+    }
+  };
+
+  let hendleSizesProduct = () => {
+    if (detailSize !== "") {
+      setSaveSize((prev) => [...prev, { size: detailSize }]);
+      setDetailSize("");
+      toast.success(
+        "Thêm dữ liệu thành công. Sau khi thêm đủ đừng quên lưu nhé"
+      );
+    } else {
+      toast.warn("Không thể thêm trường rỗng. Vui lòng thao tác lại");
+    }
+  };
+  let hendleSaveSize = () => {
+    if (saveSize && saveSize.length > 0) {
+      setProductData((prev) => ({
+        ...prev,
+        sizes: saveSize,
+      }));
+      toast.success("Lưu thành công");
+    } else {
+      toast.warn("Không thể lưu dữ liệu rỗng");
+    }
+  };
+  console.log("Xuan manh check data", productData);
   return (
     <main id="main" class="main">
       <div class="pagetitle">
@@ -52,7 +173,7 @@ function AddProduct() {
                           <option value="">Chó</option>
                           <option value="">Mèo</option>
                           <option value="">Chim</option>
-                          <option value="">Súc vật</option>
+                          <option value="">Phụ kiện cho thú cưng</option>
                         </select>
                       </div>
                       <div class="form-group">
@@ -64,6 +185,7 @@ function AddProduct() {
                           placeholder="Name"
                           required
                           name="name"
+                          onChange={handleInputChange}
                         />
                       </div>
                       <div class="form-group">
@@ -73,7 +195,8 @@ function AddProduct() {
                           class="form-control"
                           id="exampleInputName1"
                           required
-                          name="quantity"
+                          name="manyProducts"
+                          onChange={handleInputChange}
                         />
                       </div>
                       <div class="form-group">
@@ -84,6 +207,7 @@ function AddProduct() {
                           id="exampleInputName1"
                           required
                           name="price"
+                          onChange={handleInputChange}
                         />
                       </div>
                       <div class="form-group">
@@ -93,7 +217,19 @@ function AddProduct() {
                           class="form-control"
                           id="exampleInputName1"
                           required
-                          name="brand"
+                          name="salePrice"
+                          onChange={handleInputChange}
+                        />
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Trademark</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputName1"
+                          required
+                          name="trademark"
+                          onChange={handleInputChange}
                         />
                       </div>
                       <div class="form-group">
@@ -108,17 +244,61 @@ function AddProduct() {
                             type="text"
                             class="form-control file-upload-info"
                             placeholder="href Image"
+                            value={detailImages}
+                            onChange={(event) =>
+                              setDetailImage(event.target.value)
+                            }
                           />
                           <span class="input-group-append d-flex gap-2">
                             <button
                               class="file-upload-browse btn btn-gradient-primary"
                               type="button"
+                              onClick={hendleImageDetail}
                             >
                               Upload
                             </button>
                             <button
                               class="file-upload-browse btn btn-primary"
                               type="button"
+                              onClick={hendleSaveDetailImage}
+                            >
+                              Save
+                            </button>
+                          </span>
+                        </div>
+
+                        <div class="text-danger"></div>
+                      </div>
+
+                      <div class="form-group">
+                        <label>Màu sắc</label>
+                        <input
+                          type="file"
+                          name="img"
+                          class="file-upload-default"
+                        />
+                        <div class="input-group col-xs-12 d-flex gap-2">
+                          <input
+                            type="text"
+                            class="form-control file-upload-info"
+                            placeholder="color product"
+                            value={detailColor}
+                            onChange={(event) =>
+                              setDetailColor(event.target.value)
+                            }
+                          />
+                          <span class="input-group-append d-flex gap-2">
+                            <button
+                              class="file-upload-browse btn btn-gradient-primary"
+                              type="button"
+                              onClick={hendleColorDetail}
+                            >
+                              Upload
+                            </button>
+                            <button
+                              class="file-upload-browse btn btn-primary"
+                              type="button"
+                              onClick={hendleSaveColor}
                             >
                               Save
                             </button>
@@ -133,11 +313,19 @@ function AddProduct() {
                           class="form-control"
                           id="exampleTextarea1"
                           required
-                          name="mota"
+                          name="comment"
                           rows="4"
+                          onChange={handleInputChange}
                         ></textarea>
                       </div>
-
+                      <div className="form-group">
+                        <label>Content Detail</label>
+                        <MdEditor
+                          style={{ height: "500px" }}
+                          renderHTML={(text) => mdParser.render(text)}
+                          onChange={handleEditorChange}
+                        />
+                      </div>
                       <div class="image-previews"></div>
                       <div class="form-group row" id="size-container">
                         <label
@@ -150,6 +338,10 @@ function AddProduct() {
                           <select
                             class="form-control"
                             id="size-select"
+                            value={detailSize}
+                            onChange={(event) =>
+                              setDetailSize(event.target.value)
+                            }
                             required
                           >
                             <option value="xs">Size XS</option>
@@ -164,6 +356,7 @@ function AddProduct() {
                             type="button"
                             class="btn btn-gradient-primary"
                             id="add-size-btn"
+                            onClick={hendleSizesProduct}
                           >
                             Thêm Size
                           </button>
@@ -171,6 +364,7 @@ function AddProduct() {
                             type="button"
                             class="btn btn-primary"
                             id="add-size-btn"
+                            onClick={hendleSaveSize}
                           >
                             Save
                           </button>
@@ -206,6 +400,18 @@ function AddProduct() {
           </div>
         </div>
       </section>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </main>
   );
 }
