89f2d976a82c93fad67936290d92de7fa0845294
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-02-19 01:49:47 +0700

    Update and fix bug 2h

--- src/components/Content/MoreProduct/ProductItem.js
+++ src/components/Content/MoreProduct/ProductItem.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./ProductItem.module.scss";
 import clsx from "clsx";
 // import { animateScroll as scroll } from "react-scroll";
@@ -8,11 +8,15 @@ import {
   faCartPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useData } from "../../Common/DataContext";
+import { ToastContainer, toast } from "react-toastify";
+import QuickProducts from "../QuickProducts/QuickProducts";
 function ProudctItem() {
   const { typeProduct } = useParams();
-  const { setTypeParams, dataProduct } = useData();
+  const [quickView, setQuickView] = useState(false);
+  const { setTypeParams, dataProduct, updateData, yourData } = useData();
+  const getIdProduct = useRef();
   useEffect(() => {
     setTypeParams(typeProduct);
   });
@@ -21,43 +25,109 @@ function ProudctItem() {
     return amount.toLocaleString("vi-VN");
   };
 
+  const hendleAddNow = (id) => {
+    const existingProductIndex = yourData.findIndex((item) => item.id === id);
+    if (existingProductIndex !== -1) {
+      toast.warn("Sản phẩm đã tồn tài trong giỏ hàng");
+    } else {
+      updateData([
+        ...yourData,
+        {
+          id,
+          quantity: 1,
+        },
+      ]);
+      toast.success("Thêm sản phẩm thành công");
+    }
+  };
+
+  const hendleQuickView = (id) => {
+    getIdProduct.current = id;
+    setQuickView((pre) => !pre);
+  };
   return (
-    <div className={clsx(styles.containerItem)}>
-      {dataProduct &&
-        dataProduct.length > 0 &&
-        dataProduct.map((item) => (
-          <div key={item.id} className={clsx(styles.productItem)}>
-            <picture>
-              <img
-                className={clsx(styles.productItem__img)}
-                src={item.avatar}
-                alt={item.name}
-              />
-            </picture>
-            <p className={clsx(styles.productItem__text)}>{item.name}</p>
-            <div className={clsx(styles.productItem__price)}>
-              <span className={clsx(styles.productItem__price__newPrice)}>
-                {formatCurrency(item.salePrice)}
-                <FontAwesomeIcon icon={faDongSign} />
-              </span>
-              <span className={clsx(styles.productItem__price__oldPrice)}>
-                {formatCurrency(item.price)}
-                <FontAwesomeIcon icon={faDongSign} />
-              </span>
+    <>
+      <div className={clsx(styles.containerItem)}>
+        {dataProduct && dataProduct.length > 0 ? (
+          dataProduct.map((item) => (
+            <div key={item.id} className={clsx(styles.productItem)}>
+              <Link
+                className={clsx(styles.wrapITem)}
+                to={`/ProductDetail/${item.id}/${item.trademark}`}
+              >
+                <picture>
+                  <img
+                    className={clsx(styles.productItem__img)}
+                    src={item.avatar}
+                    alt={item.name}
+                  />
+                </picture>
+                <p className={clsx(styles.productItem__text)}>{item.name}</p>
+                <div className={clsx(styles.productItem__price)}>
+                  <span className={clsx(styles.productItem__price__newPrice)}>
+                    {formatCurrency(item.salePrice)}
+                    <FontAwesomeIcon icon={faDongSign} />
+                  </span>
+                  <span className={clsx(styles.productItem__price__oldPrice)}>
+                    {formatCurrency(item.price)}
+                    <FontAwesomeIcon icon={faDongSign} />
+                  </span>
+                </div>
+              </Link>
+              <div className={clsx(styles.productItem__boxmodal)}>
+                <button
+                  className={clsx(styles.productItem__boxmodal__quickView)}
+                  onClick={() => hendleQuickView(item.id)}
+                >
+                  <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
+                  <span>Xem nhanh</span>
+                </button>
+                <button
+                  onClick={() => {
+                    hendleAddNow(item.id);
+                  }}
+                  className={clsx(styles.productItem__boxmodal__addNow)}
+                >
+                  <FontAwesomeIcon icon={faCartPlus} />
+                  <span>Thêm ngay</span>
+                </button>
+              </div>
             </div>
-            <div className={clsx(styles.productItem__boxmodal)}>
-              <button className={clsx(styles.productItem__boxmodal__quickView)}>
-                <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
-                <span>Xem nhanh</span>
-              </button>
-              <button className={clsx(styles.productItem__boxmodal__addNow)}>
-                <FontAwesomeIcon icon={faCartPlus} />
-                <span>Thêm ngay</span>
-              </button>
-            </div>
+          ))
+        ) : (
+          <div className={clsx(styles.emptyProduct)}>
+            <img
+              src="https://i.ibb.co/C7CmWTT/empty-cart.png"
+              alt="emptyProduct"
+            />
+            <p>
+              Sản phẩm này hiện tại bên cửa hàng đang tạm hết. Cửa hàng sẽ cập
+              nhật sản phẩm sau, xin cảm ơn quý khách hàng.
+            </p>
           </div>
-        ))}
-    </div>
+        )}
+      </div>
+      {quickView ? (
+        <QuickProducts
+          id={getIdProduct.current}
+          hendleQuickViewProduct={hendleQuickView}
+        />
+      ) : (
+        ""
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 }
 
